7e23af4e126a6326cf7028dc8d57f5af62d844be
Victor Fomenko <viktor.fomenko@iqoption.com>
2019-07-11 11:57:52 +0300

    Step 2 — Gradient fill

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const COLOR_LINE = "#009ee3";
 const LINE_WIDTH = 2;
+const GRADIENT_START = "rgba(0, 159, 228, 0)";
+const GRADIENT_STOP = "rgba(0, 159, 228, 0.18)";
 
 const scale = window.devicePixelRatio;
 const canvas = document.getElementById("canvas");
@@ -39,11 +41,17 @@ const scaledData = candles.map(i => {
 });
 
 const ctx = canvas.getContext("2d");
+const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+gradient.addColorStop(0, GRADIENT_STOP);
+gradient.addColorStop(1, GRADIENT_START);
+
 ctx.strokeStyle = COLOR_LINE;
 ctx.lineWidth = LINE_WIDTH;
+ctx.fillStyle = gradient;
 
 scaledData.forEach(({ x, y }) => {
   ctx.lineTo(x, y);
 });
 
 ctx.stroke();
+ctx.fill();
